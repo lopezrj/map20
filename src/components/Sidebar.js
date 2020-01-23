fab0bbf672d3cc5bfe7de843b4008a46feba0f05
lopezrj <lopezrj@Mini9.local>
2020-01-23 17:10:47 -0600

    Improve sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,8 +8,8 @@ export class Sidebar extends Component {
     return (
       <div className="App-sidebar">
         <ul>
-          <li><Link to='/'>Mapa</Link></li>
-          <li><Link to='/about'>About</Link></li>
+          <Link to='/'><li>Mapa</li></Link>
+          <Link to='/about'><li>About</li></Link>
           <li>Other</li>
           <li>Other</li>
           <li>Other</li>
